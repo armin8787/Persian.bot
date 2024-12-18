2d61deb9a8872ad3aaf1c319fcd0b746f3903d20
Armin20008a <atenavatanchi87@gmail.com>
2024-12-19 02:55:52 +0330

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "674822cf6734a1bc50156da2"
+  room_id = ""
   bot_token = "050fa23e453578beb0321fbaa6d29e76dbd8b03148af11530a092aee86196c38"
   bot_file = "main"
   bot_class = "Bot"
